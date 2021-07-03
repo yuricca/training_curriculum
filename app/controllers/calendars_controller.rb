999c69e0531e19b8471d322a7c230d401dacdc5d
yuricca <yuricca531@gmail.com>
2021-07-03 17:50:06 +0900

    不要な記述の削除

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,9 +36,6 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = @todays_date.wday
-      if wday_num >= 7
-        wday_num = wday_num - 7
-      end
 
       days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[(@todays_date + x).wday]}
 
